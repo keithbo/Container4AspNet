fbc54a5764a64542d0edecd3a980b117d3388bdd
keithbo <keith.bosacker@gmail.com>
2016-04-16 13:11:54 -0500

    condense API

--- Container4AspNet.Windsor/WindsorExtensions.cs
+++ Container4AspNet.Windsor/WindsorExtensions.cs
@@ -1,12 +1,28 @@
 ﻿namespace Container4AspNet.Windsor
 {
-	using Castle.Windsor;
+    using System;
+    using Castle.Windsor;
+	using Owin;
 
-	/// <summary>
+    /// <summary>
 	/// Extension methods for Castle Windsor integration
 	/// </summary>
 	public static class WindsorExtensions
 	{
+        public static IAppBuilder UseWindsor(this IAppBuilder builder, Action<IContainerConfigurator<IWindsorContainer>> configure)
+        {
+            return UseWindsor(builder, new WindsorContainer(), configure);
+        }
+
+        public static IAppBuilder UseWindsor(this IAppBuilder builder, IWindsorContainer container, Action<IContainerConfigurator<IWindsorContainer>> configure)
+        {
+            if (container == null)
+            {
+                throw new ArgumentNullException("container");
+            }
+            return builder.UseContainer(container, new WindsorContainerConfigurator(), configure);
+        }
+
 		/// <summary>
 		/// Constructs a new IScopeResolver for a given IWindsorContainer.
 		/// </summary>
